03ead513ee69338b7cb9b8028804771de3ac5ed3
Jules ROY <julesroydev@gmail.com>
2022-06-19 16:13:50 +0200

    Ajout d'une fonction permettant de rejouer

--- Game/gamesourcecode.c
+++ Game/gamesourcecode.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int rejouer()
+{
+	int rejouer = -1;
+	printf("Rejouer ? (0 pour quitter, 1 pour rejouer)\n");
+	scanf_s("%d", &rejouer);
+	if (rejouer == 0)
+	{
+		return 0;
+	}
+	else if (rejouer == 1)
+	{
+		main();
+	}
+	else
+	{
+		printf("Cette touche n'est pas valide, merci d'entrer 0 ou 1 selon votre choix\n");
+	}
+}
+
 int main()
 {
-	printf("###Bienvenue sur le jeu###\n");
+	printf("### Bienvenue sur GuessTN ###\n");
+	printf("Pour l'utilisation de ce logiciel, merci de vous referer au fichier manual.txt fourni avec le logiciel\n");
 	int a_deviner = 0;
 	int nombre_de_coups = 10;
 	int nombre_de_vies = 10;
@@ -16,7 +37,8 @@ int main()
 	}
 	int entree_joueur_2 = 0;
 
-	while (entree_joueur_2 != a_deviner && nombre_de_vies != 0)
+	while (entree_joueur_2 != a_deviner && nombre_de_vies != 0) /*tant que l'entree du joueur 2 ne correspond pas au chiffre
+																a deviner du joueur 1 et qu'il reste des vies au joueur 2*/
 	{
 		printf("Joueur 2, cherchez le nombre mystere (compris entre 1 et 100)\n");
 		scanf_s("%d", &entree_joueur_2);
@@ -47,12 +69,13 @@ int main()
 	if (nombre_de_vies == 0)
 	{
 		printf("Dommage, le nombre mystere etait %d\n", a_deviner);
+		rejouer();
 	}
 	else
 	{
 		nombre_de_coups -= nombre_de_vies;
 		printf("Bravo, vous avez trouve !!\n");
-		printf("Il vous aura fallu %d tentatives", nombre_de_coups + 1);
+		printf("Il vous aura fallu %d tentatives\n", nombre_de_coups + 1);
+		rejouer();
 	}
-	return 0;
 }
